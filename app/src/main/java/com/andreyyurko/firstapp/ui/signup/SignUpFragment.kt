f598aa11f1863dd7250a57800a933f29177aec4f
AndreyYurko <andreyyurko01@gmail.com>
2021-10-06 14:37:11 +0300

    fix problems

--- app/src/main/java/com/andreyyurko/firstapp/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/andreyyurko/firstapp/ui/signup/SignUpFragment.kt
@@ -1,13 +1,15 @@
-package com.andreyyurko.firstapp
+package com.andreyyurko.firstapp.ui.signup
 
 import androidx.fragment.app.viewModels
-import com.andreyyurko.firstapp.databinding.FragmentSignUpBinding
+import com.andreyyurko.firstapp.R
 import com.andreyyurko.firstapp.ui.base.BaseFragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.andreyyurko.firstapp.databinding.FragmentSignUpBinding
 import com.andreyyurko.firstapp.ui.signin.SignInViewModel
 
-class SignUnFragment : BaseFragment(R.layout.fragment_sign_up) {
 
-    //private val viewBinding by viewBinding(FragmentSignUpBinding::bind)
+class SignUnFragment : BaseFragment(R.layout.fragment_sign_up) {
+    private val viewBinding by viewBinding(FragmentSignUpBinding::bind)
 
     private val viewModel: SignInViewModel by viewModels()
 }
